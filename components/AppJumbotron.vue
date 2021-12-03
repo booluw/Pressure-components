f6b10225f80e49f88aaff0662931df97017277f4
= <=>
2021-12-03 13:54:40 +0100

    :star: Completed the Homepage, still got some few issues.

--- components/AppJumbotron.vue
+++ components/AppJumbotron.vue
@@ -7,7 +7,7 @@
         </h1>
         <div class="row m-0 justify-content-between" style="text-align: center; margin: 3rem 0 0;">
           <div class="col-md-5 p-0">
-            <a href="~/static/doc/PCI-HOSE-FITTING-LIST-PRICING-2021.pdf" class="btn btn--big btn--bordered" download>New Custom Fitting Line</a>
+            <a href="docs/PCI-HOSE-FITTING-LIST-PRICING-2021.pdf" class="btn btn--big btn--bordered" download>New Custom Fitting Line</a>
           </div>
           <div class="col-md-5 p-0">
             <nuxt-link to="stocking-in-mexico"  class="btn btn--big btn--bordered" download>Now Stocking in Mexico</nuxt-link>
@@ -15,7 +15,7 @@
         </div>
       </div>
       <div class="col-md-3 p-0">
-        <a href="~/static/docs/2021-PCI-Catalog-update-Sept-digital-compressed.pdf" download>
+        <a href="docs/2021-PCI-Catalog-update-Sept-digital-compressed.pdf" download>
           <img src="~/static/img/Click-to-View-the-2021-Catalog.png" class="jumbotron__img" />
         </a>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,66 @@
 <template>
   <section>
     <app-jumbotron />
+    <section class="section section--center">
+      <h2 class="section__heading">
+        Introducing our new Hose Fitting Line
+      </h2>
+      <div class="row m-0 justify-content-center pb-5">
+        <div class="col-md-4 p-0">
+          <a href="docs/PCI-HOSE-FITTING-LIST-PRICING-2021.pdf" class="btn btn--big btn--bordered" download>New Custom Fitting Line</a>
+        </div>
+      </div>
+      <div class="row m-0 justify-content-center">
+        <div class="col-md-11 p-0">          
+          <a href="docs/PCI-HOSE-FITTING-LIST-PRICING-2021.pdf" download>
+            <img src="~/static/img/Hose-Fitting-Line-1024x644.png"/>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="section" style="padding-top: 3rem;padding-bottom: 3rem;">
+      <div class="row m-0 align-items-center">
+        <div class="col-6 p-0">
+          <h2 class="section__heading">
+            SERVICES & SOLUTIONS
+          </h2>
+        </div>
+        <div class="col-6 p-0">
+          <p class="section__paragraph section_paragraph--border">
+            We are a leading supplier of quality hydraulic components including high pressure ball valves, quick disconnect couplings, diagnostic products and pipe, tube and hose clamps.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="row m-0 align-items-center">
+        <div class="col-md-1 p-0"></div>
+        <div class="col-md-6 p-0">
+          <h2 class="section__heading section__heading--lato">
+            THE NEXT BEST THING IN<br />
+            <b>HYDRAULIC EQUIPMENT</b>
+          </h2>
+        </div>
+        <div class="col-md-5 p-0">
+          <img src="~/static/img/indusry-wp-img.jpg" class="section__img" />
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="row m-0">
+        <div class="col-md-6 p-0">
+          <img src="~/static/img/pci-stock-1-min.png" class="section__img" />
+        </div>
+        <div class="col-md-6 p-0">
+          <h2 class="section__heading section__heading--small">
+            Our Mission
+          </h2>
+          <p class="section__paragraph">
+            Connecting our Customers with the hydraulic components that make their business run
+          </p>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
