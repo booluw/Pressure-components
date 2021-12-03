edc936ca96328a6cbde372f4dbe2ee0864fe92ad
= <=>
2021-12-03 14:41:26 +0100

    :star: Completed AboutUs page.

---
+++ pages/about-us.vue
@@ -0,0 +1,112 @@
+<template>
+  <section>
+    <section class="jumbotron jumbotron--center" style="background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url('img/IMG_4171-1-1024x603.jpg');">
+      <h1 class="jumbotron__heading">About Us</h1>
+    </section>
+    <section class="section">
+      <div class="row m-0 align-items-center">
+        <div class="col-6 p-0">
+          <h2 class="section__heading">
+            WHY CHOOSE US
+          </h2>
+        </div>
+        <div class="col-6 p-0">
+          <p class="section__paragraph section_paragraph--border">
+            Pressure Components, Inc. was established in 1985. We are a leading supplier of quality hydraulic components including high pressure ball valves, quick disconnect couplings, diagnostic products and pipe, tube and hose clamps.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="section" style="padding: 3rem 6rem;">
+      <div class="row m-0 align-items-stretch">
+        <div class="col-md-4 p-0">
+          <div class="card">
+            <i class="card__icon fa fa-user"></i>
+            <div class="card__text">
+              <h3 class="card__heading">Complete Interchange</h3>
+              <p class="card__paragraph">
+                We have the most complete interchange available in our quick coupling and clamp lines. Coupling sizes from 1/8” to 2” with various threads and clamp sizes from 6MM to 8” pipe.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card">
+            <i class="card__icon fa fa-bullseye"></i>
+            <div class="card__text">
+              <h3 class="card__heading">Quality First</h3>
+              <p class="card__paragraph">
+                We only partner with the best quality manufacturers in the industry.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card">
+            <i class="card__icon fa fa-calendar"></i>
+            <div class="card__text">
+              <h3 class="card__heading">Warranty</h3>
+              <p class="card__paragraph">
+                We offer a full one year warranty on all our products.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card">
+            <i class="card__icon fa fa-users"></i>
+            <div class="card__text">
+              <h3 class="card__heading">Engineering & Testing</h3>
+              <p class="card__paragraph">
+                We have full engineering and testing capabilities.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card">
+            <i class="card__icon fa fa-fast-forward"></i>
+            <div class="card__text">
+              <h3 class="card__heading">Certified</h3>
+              <p class="card__paragraph">
+                Our high pressure ball valve line carries multiple cerifications including ISO-9001, Lloyd’s register, DNV, TUV and DVGW
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card">
+            <i class="card__icon fa fa-life-ring"></i>
+            <div class="card__text">
+              <h3 class="card__heading">Priority</h3>
+              <p class="card__paragraph">
+                Every order at PCI is a priority
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="row m-0">
+        <div class="col-md-6 p-0">
+          <img src="~/static/img/IMG_4171-1-1024x603.jpg" class="section__img" />
+        </div>
+        <div class="col-md-6 p-0">
+          <h2 class="section__heading section__heading--small">
+            Our Experience
+          </h2>
+          <p class="section__paragraph">
+            Our expertise is put to work for our customers every time. We are ready to address each problem and provide you with the best solution.
+          </p>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutUsPage'
+}
+</script>
