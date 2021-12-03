8d4fc86f4d119dfa2a28f2157854e60711074830
= <=>
2021-12-03 16:30:12 +0100

    :star: Completed IndustriesPage view.

---
+++ pages/industries.vue
@@ -0,0 +1,82 @@
+<template>
+  <section>
+    <section class="jumbotron" style="background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url('img/bc1.jpg');background-size: 100% 100%;height: 40vh;">
+      <div class="row m-0 align-items-middle" style="height: 100%">
+        <div class="col-md-7 p-0">
+          <h1 class="jumbotron__heading">Industries We Work With</h1>
+          <h2 class="section__heading section__heading--lato">
+            <b>Stratedgy and Innovation</b>
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <h1 class="section__heading section__heading--lato">
+        Serving the industry with quality, price competitive products since 1985
+      </h1>
+      <br />
+      <div class="row m-0 align-items-strech justify-content-center">
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/heavy-industrial-chemical-factory-at-night.jpg" class="card__img"/>
+            <h3 class="card__heading">Industrial</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/agricultural.jpg" class="card__img"/>
+            <h3 class="card__heading">Agricultural</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/offshore.jpg" class="card__img"/>
+            <h3 class="card__heading">Offshore</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/sand-mining.jpg" class="card__img"/>
+            <h3 class="card__heading">Mining</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/construction.jpg" class="card__img"/>
+            <h3 class="card__heading">Construction</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/driveway.jpg" class="card__img"/>
+            <h3 class="card__heading">Pressure washing</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/TASNEE.jpg" class="card__img"/>
+            <h3 class="card__heading">Petrochemical</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/pexels.jpeg" class="card__img"/>
+            <h3 class="card__heading">Renewable Energies</h3>
+          </div>
+        </div>
+        <div class="col-md-3 p-0">
+          <div class="card card--with-img">
+            <img src="~/static/img/ser2.jpg" class="card__img"/>
+            <h3 class="card__heading">Others</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'IndustriesPage'
+}
+</script>
